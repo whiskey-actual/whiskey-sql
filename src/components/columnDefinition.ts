d277d5705e53b458b0a10642adae664b70f27a32
Whiskey Actual <actualwhiskey@gmail.com>
2023-11-06 13:20:38 -0500

    refactor into logical components

--- src/components/columnDefinition.ts
+++ src/components/columnDefinition.ts
@@ -1,12 +1,12 @@
 import mssql from 'mssql'
 
 export class ColumnDefinition {
-    constructor(columnName:string, columnType:mssql.ISqlTypeFactoryWithNoParams|mssql.ISqlTypeFactoryWithLength, isNullable:boolean=true) {
+    constructor(columnName:string, columnType:mssql.ISqlTypeFactoryWithNoParams|mssql.ISqlTypeWithLength, isNullable:boolean=true) {
         this.columnName=columnName
         this.columnType=columnType
         this.isNullable=isNullable
     }
     public columnName:string=''
-    public columnType:mssql.ISqlTypeFactoryWithNoParams=mssql.Int
+    public columnType:mssql.ISqlTypeFactoryWithNoParams|mssql.ISqlTypeWithLength=mssql.Int
     public isNullable:boolean=false
 }
