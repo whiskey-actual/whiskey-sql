fdb2abe63318feb2fffc375aef652d4cc063f2b6
Whiskey Actual <actualwhiskey@gmail.com>
2023-11-06 10:02:34 -0500

    refactor into logical components

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,50 @@
 import { LogEngine } from 'whiskey-log';
 
 import { CreateTable } from './create';
-import { Update } from './update'
+import { UpdateTable, RowUpdate, ColumnUpdate } from './update'
+import { selectColumns } from './get';
+
+import { ColumnValuePair } from './components/columnValuePair';
 
 import mssql from 'mssql'
 
 export class DBEngine {
 
     constructor(logEngine:LogEngine, sqlConfig:any) {
-        this._le = logEngine;
-        this._sqlPool = new mssql.ConnectionPool(sqlConfig)
-        this._le.AddLogEntry(LogEngine.EntryType.Success, `DBEngine initialized ( don't forget to call connect()! )`)
+        this.le = logEngine;
+        this.sqlPool = new mssql.ConnectionPool(sqlConfig)
+        this.le.AddLogEntry(LogEngine.EntryType.Success, `DBEngine initialized ( don't forget to call connect()! )`)
         
     }
-    private _le:LogEngine
-    private _sqlPool:mssql.ConnectionPool
+    private le:LogEngine
+    private sqlPool:mssql.ConnectionPool
 
     public async connect() {
-        this._le.AddLogEntry(LogEngine.EntryType.Info, `connecting to mssql ..`)
-        await this._sqlPool.connect()
-        this._le.AddLogEntry(LogEngine.EntryType.Success, `.. connected.`)
+        this.le.AddLogEntry(LogEngine.EntryType.Info, `connecting to mssql ..`)
+        await this.sqlPool.connect()
+        this.le.AddLogEntry(LogEngine.EntryType.Success, `.. connected.`)
     }
 
     public async disconnect() {
-        this._le.AddLogEntry(LogEngine.EntryType.Info, `disconnecting from mssql ..`)
-        await this._sqlPool.close()
-        this._le.AddLogEntry(LogEngine.EntryType.Success, `.. disconnected.`)
+        this.le.AddLogEntry(LogEngine.EntryType.Info, `disconnecting from mssql ..`)
+        await this.sqlPool.close()
+        this.le.AddLogEntry(LogEngine.EntryType.Success, `.. disconnected.`)
+    }
+
+    public async selectColumns(objectName:string, columns:string[], matchConditions:ColumnValuePair[]) {
+        selectColumns(this.le, this.sqlPool, objectName, columns, matchConditions)
     }
 
 }
 
 module.exports = {
+    ColumnValuePair,
+    // creation
     CreateTable,
-    Update
+    // updates
+    UpdateTable,
+    RowUpdate,
+    ColumnUpdate,
+    // read
+    selectColumns
 }
--- src/update.ts
+++ src/update.ts
@@ -2,106 +2,92 @@ import mssql from 'mssql'
 import { LogEngine } from 'whiskey-log';
 import { SqlStatement } from './execute';
 
-export class Update {
-    constructor(logEngine:LogEngine, sqlConfig:any, tableName:string, primaryKeyColumnName:string) {
-        this.le = logEngine
-        this.sqlPool = new mssql.ConnectionPool(sqlConfig)
-        this.tableName=tableName
-        this.primaryKeyColumnName=primaryKeyColumnName
-    }
-    private le:LogEngine
-    private sqlPool:mssql.ConnectionPool
-    public tableName:string = ''
-    public primaryKeyColumnName:string = ''
-    public RowUpdates:RowUpdate[] = []
 
-    public async execute(changeDetection:boolean=false):Promise<void> {
-        this.le.logStack.push(this.tableName);
-        this.le.AddLogEntry(LogEngine.EntryType.Debug, `updating ${this.RowUpdates.length} rows on \x1b[96m${this.tableName}\x1b[0m`)
-        try {
+export async function UpdateTable(le:LogEngine, sqlPool:mssql.ConnectionPool, tableName:string, primaryKeyColumnName:string, rowUpdates:RowUpdate[], changeDetection:boolean=false):Promise<void> {
+    le.logStack.push(tableName);
+    le.AddLogEntry(LogEngine.EntryType.Debug, `updating ${rowUpdates.length} rows on \x1b[96m${tableName}\x1b[0m`)
+    try {
 
-            for(let i=0; i<this.RowUpdates.length; i++) {
+        for(let i=0; i<rowUpdates.length; i++) {
 
-                let selectQuery = 'SELECT '
+            let selectQuery = 'SELECT '
 
-                // iterate through the column updates to build the select statement
-                for(let j=0; j<this.RowUpdates[i].ColumnUpdates.length; j++) {
-                    selectQuery += this.RowUpdates[i].ColumnUpdates[j].ColumnName
-                    if(j<this.RowUpdates[i].ColumnUpdates.length-1) {selectQuery += ','}
-                    selectQuery += ' '
-                }
+            // iterate through the column updates to build the select statement
+            for(let j=0; j<rowUpdates[i].ColumnUpdates.length; j++) {
+                selectQuery += rowUpdates[i].ColumnUpdates[j].ColumnName
+                if(j<rowUpdates[i].ColumnUpdates.length-1) {selectQuery += ','}
+                selectQuery += ' '
+            }
 
-                selectQuery += `FROM ${this.tableName} WHERE ${this.primaryKeyColumnName}=@PrimaryKeyValue`
+            selectQuery += `FROM ${tableName} WHERE ${primaryKeyColumnName}=@PrimaryKeyValue`
 
-                //this.le.AddLogEntry(LogEngine.EntryType.Debug, LogEngine.EntryType.Note, selectQuery);
+            //this.le.AddLogEntry(LogEngine.EntryType.Debug, LogEngine.EntryType.Note, selectQuery);
 
-                const r = this.sqlPool.request()
-                r.input('PrimaryKeyValue', mssql.Int, this.RowUpdates[i].primaryKeyValue)
+            const r = sqlPool.request()
+            r.input('PrimaryKeyValue', mssql.Int, rowUpdates[i].primaryKeyValue)
 
-                const result = await SqlStatement(this.le, this.sqlPool, selectQuery, r)
-                
-                let columnUpdateStatements:string[] = []
+            const result = await SqlStatement(le, sqlPool, selectQuery, r)
+            
+            let columnUpdateStatements:string[] = []
 
-                const updateRequest = this.sqlPool.request()
-                updateRequest.input('PrimaryKeyValue', mssql.Int, this.RowUpdates[i].primaryKeyValue)
-                
-                // iterate over the column updates again to compare values, and build the update statement
-                for(let j=0; j<this.RowUpdates[i].ColumnUpdates.length; j++) {
+            const updateRequest = sqlPool.request()
+            updateRequest.input('PrimaryKeyValue', mssql.Int, rowUpdates[i].primaryKeyValue)
+            
+            // iterate over the column updates again to compare values, and build the update statement
+            for(let j=0; j<rowUpdates[i].ColumnUpdates.length; j++) {
 
-                    const currentValue:any = result.recordset[0][this.RowUpdates[i].ColumnUpdates[j].ColumnName]
-                    const newValue:any = this.RowUpdates[i].ColumnUpdates[j].ColumnValue
+                const currentValue:any = result.recordset[0][rowUpdates[i].ColumnUpdates[j].ColumnName]
+                const newValue:any = rowUpdates[i].ColumnUpdates[j].ColumnValue
 
-                    if((newValue && !currentValue) || (newValue && currentValue && newValue.toString().trim()!==currentValue.toString().trim()))
-                    {
-                            // dont log timestamp changes, because they are expected on nearly every update.
-                            if(this.RowUpdates[i].ColumnUpdates[j].ColumnType!==mssql.DateTime2) {
-                                this.le.AddLogEntry(LogEngine.EntryType.Change, `\x1b[96m${this.tableName}\x1b[0m.\x1b[96m${this.RowUpdates[i].ColumnUpdates[j].ColumnName}\x1b[0m: "\x1b[96m${currentValue}\x1b[0m"->"\x1b[96m${newValue}\x1b[0m".. `, this.RowUpdates[i].updateName)
-                            }
-                            columnUpdateStatements.push(`${this.RowUpdates[i].ColumnUpdates[j].ColumnName}=@${this.RowUpdates[i].ColumnUpdates[j].ColumnName}`)
-                            updateRequest.input(this.RowUpdates[i].ColumnUpdates[j].ColumnName, this.RowUpdates[i].ColumnUpdates[j].ColumnType, this.RowUpdates[i].ColumnUpdates[j].ColumnValue)
-                    }
-                }
-
-                // do we have updates to perform?
-                if(columnUpdateStatements.length>0) {
-
-                    let updateStatement:string = `UPDATE ${this.tableName} SET `
-
-                    for(let j=0; j<columnUpdateStatements.length; j++) {
-                        updateStatement += columnUpdateStatements[j]
-                        if(j<columnUpdateStatements.length-1) { updateStatement += ','}
-                        updateStatement += ' '
-                    }
-
-                    updateStatement += `WHERE ${this.primaryKeyColumnName}=@PrimaryKeyValue`
-
-                    try {
-                        await SqlStatement(this.le, this.sqlPool, updateStatement, updateRequest)
-                    } catch(err) {
-                        this.le.AddLogEntry(LogEngine.EntryType.Error, updateStatement);
-                        this.le.AddLogEntry(LogEngine.EntryType.Error, `${err}`);
-                        console.debug(this)
-                        for(let i=0; i<this.RowUpdates.length; i++) {
-                            for(let j=0; j<this.RowUpdates[i].ColumnUpdates.length; j++) {
-                                console.debug(this.RowUpdates[i].ColumnUpdates[j])
-                            }
+                if((newValue && !currentValue) || (newValue && currentValue && newValue.toString().trim()!==currentValue.toString().trim()))
+                {
+                        // dont log timestamp changes, because they are expected on nearly every update.
+                        if(rowUpdates[i].ColumnUpdates[j].ColumnType!==mssql.DateTime2) {
+                            le.AddLogEntry(LogEngine.EntryType.Change, `\x1b[96m${tableName}\x1b[0m.\x1b[96m${rowUpdates[i].ColumnUpdates[j].ColumnName}\x1b[0m: "\x1b[96m${currentValue}\x1b[0m"->"\x1b[96m${newValue}\x1b[0m".. `, rowUpdates[i].updateName)
                         }
-                        throw(err)
-                    }
+                        columnUpdateStatements.push(`${rowUpdates[i].ColumnUpdates[j].ColumnName}=@${rowUpdates[i].ColumnUpdates[j].ColumnName}`)
+                        updateRequest.input(rowUpdates[i].ColumnUpdates[j].ColumnName, rowUpdates[i].ColumnUpdates[j].ColumnType, rowUpdates[i].ColumnUpdates[j].ColumnValue)
                 }
             }
-        } catch(err) {
-            this.le.AddLogEntry(LogEngine.EntryType.Error, `${err}`)
-            throw(err)
-        } finally {
-            //this.le.AddLogEntry(LogEngine.EntryType.Info, LogEngine.EntryType.Success, Utilities.getProgressMessage(updatePackage.tableName, 'persisted', updatePackage.UpdatePackageItems.length, updatePackage.UpdatePackageItems.length, startDate, new Date))
-            this.le.logStack.pop()
+
+            // do we have updates to perform?
+            if(columnUpdateStatements.length>0) {
+
+                let updateStatement:string = `UPDATE ${tableName} SET `
+
+                for(let j=0; j<columnUpdateStatements.length; j++) {
+                    updateStatement += columnUpdateStatements[j]
+                    if(j<columnUpdateStatements.length-1) { updateStatement += ','}
+                    updateStatement += ' '
+                }
+
+                updateStatement += `WHERE ${primaryKeyColumnName}=@PrimaryKeyValue`
+
+                try {
+                    await SqlStatement(le, sqlPool, updateStatement, updateRequest)
+                } catch(err) {
+                    le.AddLogEntry(LogEngine.EntryType.Error, updateStatement);
+                    le.AddLogEntry(LogEngine.EntryType.Error, `${err}`);
+                    for(let i=0; i<rowUpdates.length; i++) {
+                        for(let j=0; j<rowUpdates[i].ColumnUpdates.length; j++) {
+                            console.debug(rowUpdates[i].ColumnUpdates[j])
+                        }
+                    }
+                    throw(err)
+                }
+            }
         }
-
-        return new Promise<void>((resolve) => {resolve()})
-
+    } catch(err) {
+        le.AddLogEntry(LogEngine.EntryType.Error, `${err}`)
+        throw(err)
+    } finally {
+        //this.le.AddLogEntry(LogEngine.EntryType.Info, LogEngine.EntryType.Success, Utilities.getProgressMessage(updatePackage.tableName, 'persisted', updatePackage.UpdatePackageItems.length, updatePackage.UpdatePackageItems.length, startDate, new Date))
     }
+
+    return new Promise<void>((resolve) => {resolve()})
+
 }
+
 
 export class RowUpdate {
     constructor(primaryKeyValue:number) {
