d542c059af88eba33d317f5f97a852e2491f539a
Whiskey Actual <actualwhiskey@gmail.com>
2023-11-06 14:50:55 -0500

    add table creation

--- src/components/columnDefinition.ts
+++ src/components/columnDefinition.ts
@@ -1,10 +1,11 @@
 import mssql from 'mssql'
 
 export class ColumnDefinition {
-    constructor(columnName:string, columnType:mssql.ISqlTypeFactoryWithNoParams|mssql.ISqlTypeWithLength, isNullable:boolean=true) {
+    constructor(columnName:string, columnType:mssql.ISqlTypeFactoryWithNoParams|mssql.ISqlTypeWithLength, isNullable:boolean=true, isIndexed:boolean=false) {
         this.columnName=columnName
         this.columnType=columnType
         this.isNullable=isNullable
+        this.isIndexed=isIndexed
     }
     public columnName:string=''
     public columnType:mssql.ISqlTypeFactoryWithNoParams|mssql.ISqlTypeWithLength=mssql.Int
--- src/components/createTable.ts
+++ src/components/createTable.ts
@@ -17,7 +17,7 @@ export async function CreateTable(le:LogEngine, sqlPool:mssql.ConnectionPool, ta
         t.columns.add(`${tableName}ID`, mssql.Int, {nullable:false, primary: true, identity: true})
         t.columns.add(`${tableName}Description`, mssql.VarChar(255), {nullable:true})
         for(let i=0; i<columnDefinitions.length; i++) {
-            t.columns.add(columnDefinitions[i].columnName, columnDefinitions[i].columnType, {nullable:columnDefinitions[i].isNullable})
+            t.columns.add(`${tableName}${columnDefinitions[i].columnName}`, columnDefinitions[i].columnType, {nullable:columnDefinitions[i].isNullable})
 
             if(columnDefinitions[i].isIndexed) {
                 indexesToCreate.push(`CREATE INDEX IDX_${tableName}_${columnDefinitions[i].columnName} ON ${tableName}(${columnDefinitions[i].columnName});`)
